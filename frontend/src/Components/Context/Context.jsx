3079e802d7263a5b9bf7ac0e294b62e973ebfc16
SAI <144314783+sainaresh2727@users.noreply.github.com>
2025-11-20 10:42:01 +0530

    Update Context.jsx
    55

--- frontend/src/Components/Context/Context.jsx
+++ frontend/src/Components/Context/Context.jsx
@@ -12,15 +12,13 @@ function Context({children}) {
   async function UrlDatas(e) {
     e.preventDefault()
     try{
-<<<<<<< HEAD
-<<<<<<< HEAD
+
         let UrlDatas=await Axios.post('https://aganithatask-backend.onrender.com/api/UrlDatas',{LongUrl,ShortCode}) 
-=======
+
         let UrlDatas=await Axios.post('https://aganitha-task-backend.vercel.app/api/UrlDatas',{LongUrl,ShortCode}) 
->>>>>>> 0a7f5b82465f1a461c8a027b51da21fbf58f7a5f
-=======
+
         let UrlDatas=await Axios.post('https://aganithatask-backend.onrender.com/api/UrlDatas',{LongUrl,ShortCode}) 
->>>>>>> 49d2bd5445747ab21f53c879efd4b7ac621b8b4d
+
         alert(UrlDatas.data.message)
         setLongUrl("")
         setShortCode("")
@@ -36,15 +34,13 @@ function Context({children}) {
   let [LongUrlData,setLongUrlData]=useState([])
   async function GetUrlDatas() {
    try{
-<<<<<<< HEAD
-<<<<<<< HEAD
+
     let Url=await Axios.get('https://aganithatask-backend.onrender.com/api/GetUrlData')
-=======
+
     let Url=await Axios.get('https://aganitha-task-backend.vercel.app/api/GetUrlData')
->>>>>>> 0a7f5b82465f1a461c8a027b51da21fbf58f7a5f
-=======
+
     let Url=await Axios.get('https://aganithatask-backend.onrender.com/api/GetUrlData')
->>>>>>> 49d2bd5445747ab21f53c879efd4b7ac621b8b4d
+
     setLongUrlData(Url.data.data)
    }
    catch(err){
@@ -60,15 +56,13 @@ function Context({children}) {
 
   async function DeleteLink(code) {
      if (!window.confirm("Delete this link?")) return;
-<<<<<<< HEAD
-<<<<<<< HEAD
+
      let Delete=await Axios.delete(`https://aganithatask-backend.onrender.com/api/DeleteUrl/${code}`)
-=======
+
      let Delete=await Axios.delete(`https://aganitha-task-backend.vercel.app/api/DeleteUrl/${code}`)
->>>>>>> 0a7f5b82465f1a461c8a027b51da21fbf58f7a5f
-=======
+
      let Delete=await Axios.delete(`https://aganithatask-backend.onrender.com/api/DeleteUrl/${code}`)
->>>>>>> 49d2bd5445747ab21f53c879efd4b7ac621b8b4d
+
      window.location.reload();
      alert(Delete.data.message)
      GetUrlDatas()
